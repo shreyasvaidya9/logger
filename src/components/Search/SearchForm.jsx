5cfe78969629cf2803e7d04153754e2d4288302a
Shreyas Vaidya <shreyas.vaidya@sunking.com>
2023-01-09 16:24:18 +0530

    1. Updated the Select inputs 2. Added comments to explain each functionality 3. Removed the unused imports

--- src/components/Search/SearchForm.jsx
+++ src/components/Search/SearchForm.jsx
@@ -1,13 +1,22 @@
 import React, { useMemo, memo } from "react";
-import { Stack, TextField, Button, Autocomplete } from "@mui/material";
+import {
+  Stack,
+  TextField,
+  Button,
+  InputLabel,
+  FormControl,
+  Select,
+  MenuItem,
+} from "@mui/material";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-import { formActions, formLabels } from "../../constants/form";
+import { formActions, formLabels, NOT_SELECTED } from "../../constants/form";
 
-const SearchForm = ({ formData, dispatchForm, filterSearch, tableData }) => {
+const SearchForm = ({ formData, dispatchForm, filterSearch, data }) => {
   const changeHandler = (action) => (event) => {
+    // Update the form values
     if (action.includes("Date")) {
       return dispatchForm({ type: action, payload: event });
     }
@@ -18,30 +27,27 @@ const SearchForm = ({ formData, dispatchForm, filterSearch, tableData }) => {
     });
   };
 
-  const changeAutocompleteHandler = (action) => (event, newValue) => {
-    return dispatchForm({
-      type: action,
-      payload: newValue || null,
-    });
-  };
-
   const submitHandler = (event) => {
     event.preventDefault();
-    filterSearch(false);
+    filterSearch();
   };
 
+  // Created the input options for the application type input
   const applicationTypeOptions = useMemo(
     () => [
-      ...new Set(tableData.map((td) => td?.applicationType).filter((td) => td)),
+      NOT_SELECTED,
+      ...new Set(data?.map((td) => td?.applicationType).filter((td) => td)),
     ],
-    [tableData]
+    [data, NOT_SELECTED]
   );
 
+  // Created the input options for the action type input
   const actionTypeOptions = useMemo(
     () => [
-      ...new Set(tableData.map((td) => td?.actionType).filter((td) => td)),
+      NOT_SELECTED,
+      ...new Set(data?.map((td) => td?.actionType).filter((td) => td)),
     ],
-    [tableData]
+    [data, NOT_SELECTED]
   );
 
   return (
@@ -52,31 +58,51 @@ const SearchForm = ({ formData, dispatchForm, filterSearch, tableData }) => {
           value={formData.logId}
           onChange={changeHandler(formActions.LOG_ID)}
           variant="outlined"
+          sx={{ width: "200px" }}
         />
         <TextField
           label={formLabels.APPLICATION_ID}
           value={formData.applicationId}
           onChange={changeHandler(formActions.APPLICATION_ID)}
           variant="outlined"
+          sx={{ width: "200px" }}
         />
-        <Autocomplete
-          options={applicationTypeOptions}
-          inputValue={formData.applicationType}
-          onChange={changeAutocompleteHandler(formActions.APPLICATION_TYPE)}
-          sx={{ width: "195px" }}
-          renderInput={(params) => (
-            <TextField {...params} label={formLabels.APPLICATION_TYPE} />
-          )}
-        />
-        <Autocomplete
-          options={actionTypeOptions}
-          inputValue={formData.actionType}
-          onChange={changeAutocompleteHandler(formActions.ACTION_TYPE)}
-          sx={{ width: "195px" }}
-          renderInput={(params) => (
-            <TextField {...params} label={formLabels.ACTION_TYPE} />
-          )}
-        />
+        <FormControl>
+          <InputLabel id={formActions.APPLICATION_TYPE}>
+            {formLabels.APPLICATION_TYPE}
+          </InputLabel>
+          <Select
+            labelId={formActions.APPLICATION_TYPE}
+            label={formLabels.APPLICATION_TYPE}
+            value={formData.applicationType}
+            onChange={changeHandler(formActions.APPLICATION_TYPE)}
+            sx={{ width: "200px" }}
+          >
+            {applicationTypeOptions.map((appType) => (
+              <MenuItem key={appType} value={appType}>
+                {appType}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <InputLabel id={formActions.ACTION_TYPE}>
+            {formLabels.ACTION_TYPE}
+          </InputLabel>
+          <Select
+            labelId={formActions.ACTION_TYPE}
+            label={formLabels.ACTION_TYPE}
+            value={formData.actionType}
+            onChange={changeHandler(formActions.ACTION_TYPE)}
+            sx={{ width: "200px" }}
+          >
+            {actionTypeOptions.map((actionType) => (
+              <MenuItem key={actionType} value={actionType}>
+                {actionType}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <DatePicker
             label={formLabels.FROM_DATE}
